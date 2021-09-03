02f0305f70c3d5d3aa9d449b26e53768a53c0122
Serega-vlk <69846480+Serega-vlk@users.noreply.github.com>
2021-09-03 12:19:27 +0300

    UserServicesTest

--- src/test/java/com/example/demo/services/UserServicesTest.java
+++ src/test/java/com/example/demo/services/UserServicesTest.java
@@ -1,4 +1,41 @@
 package com.example.demo.services;
 
+import com.example.demo.entity.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+@SpringBootTest
 public class UserServicesTest {
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public void getUserByUsernameTest(){
+        User user = userService.getUserByUsername("admin");
+        Assertions.assertEquals(user.getName(), "Сергей Волков");
+    }
+
+    @Test
+    public void isUserIsAdminTest(){
+        Assertions.assertTrue(userService.isUserIsAdmin("admin"));
+    }
+
+    @Test
+    public void isBlockedTest(){
+        User user = userService.getUserByUsername("admin");
+        Assertions.assertFalse(userService.isBlocked(user));
+    }
+
+    @Test
+    public void hasUsernameTest(){
+        Assertions.assertTrue(userService.hasUsername("admin"));
+    }
+
+    @Test
+    public void hasEmailTest(){
+        Assertions.assertTrue(userService.hasEmail("cjj200@gmail.com"));
+    }
 }
